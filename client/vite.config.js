75ab4d5d4a9d400184f464d1e63537377fb64e59
StarGeek06 <mahutonadisso@gmail.com>
2024-06-30 08:09:56 +0100

    Ajout de l'API pour le projet

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // Specify the correct entry point for your application
-      input: 'App.jsx' // Replace with your actual entry point file
-    }
- }
 })
 
