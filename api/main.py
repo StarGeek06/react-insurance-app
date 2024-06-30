606945f7784999f38bedbe78de4f884963bccbff
StarGeek06 <mahutonadisso@gmail.com>
2024-06-30 20:27:44 +0100

    Modification de main.py

--- api/main.py
+++ api/main.py
@@ -13,14 +13,14 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://react-insurance-app.onrender.com"
+    "https://react-insurance-app.onrender.com/"
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["POST"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
