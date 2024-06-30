bcafdeb48db715e1f2735793e9fd1d60c33149c1
StarGeek06 <mahutonadisso@gmail.com>
2024-06-30 20:48:51 +0100

    Modification de main.py

--- api/main.py
+++ api/main.py
@@ -10,17 +10,19 @@ model = joblib.load('random_forest_insurance.pkl')
 
 app = FastAPI()
 
+# Liste des origines autorisées pour CORS
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://react-insurance-app.onrender.com/"
+    "https://react-insurance-app.onrender.com"
 ]
 
+# Middleware CORS pour autoriser les origines spécifiées
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
 
@@ -39,10 +41,13 @@ def home():
 def predict(data: InputData, response: Response):
     input_data = np.array([[data.age, data.bmi, data.children, data.smoker, data.region]])
     prediction = model.predict(input_data)
+    
+    # Définition des en-têtes de réponse appropriés
     response.headers["Cache-Control"] = "no-cache, max-age=0"
     response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "0"
     response.headers["x-content-type-options"] = "nosniff"
+    response.headers["Access-Control-Allow-Origin"] = "https://react-insurance-app.onrender.com"
+
     return {'charge': prediction[0]}
 
 if __name__ == '__main__':
